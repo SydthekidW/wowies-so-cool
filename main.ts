b2533fd6eef356f01c6c5dd830add30f6b754b6f
SydthekidW <151663199+SydthekidW@users.noreply.github.com>
2023-11-21 11:49:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,2 @@
+music.play(music.stringPlayable("A D A F G E G D ", 500), music.PlaybackMode.LoopingInBackground)
+music.play(music.stringPlayable("C5 E A D F A C D ", 500), music.PlaybackMode.LoopingInBackground)
